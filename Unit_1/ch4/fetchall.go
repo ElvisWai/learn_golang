ea75796069778d617452f531017c41a0448562e6
liziwei <743621571@qq.com>
2021-06-30 21:39:26 +0800

    version 1.0.0 - 学习进度“1.7. Web服务”

--- Unit_1/ch4/fetchall.go
+++ Unit_1/ch4/fetchall.go
@@ -1,7 +1,10 @@
+// Fetchall fetches URLs in parallel and reports their times and sizes.
 package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -11,6 +14,8 @@ func main() {
 	start := time.Now()
 	ch := make(chan string)
 	for _, url := range os.Args[1:] {
+		// go这个关键字创建一个goroutine，并且让函数在这个goroutine异步执行http.Get方法
+		// main函数本身也运行在一个goroutine中，而go function则表示创建一个新的goroutine
 		go fetch(url, ch)
 	}
 	for range os.Args[1:] {
@@ -26,14 +31,15 @@ func fetch(url string, ch chan<- string) {
 		ch <- fmt.Sprint(err)
 		return
 	}
-	//_, err := io.Copy(ioutil.Discard, resp.Body)
+	// io.Copy会把响应的Body内容拷贝到ioutil.Discard输出流中
+	//（译注：可以把这个变量看作一个垃圾桶，可以向里面写一些不需要的数据），
+	// 因为我们需要这个方法返回的字节数，但是又不想要其内容。
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close()
 	if err != nil {
 		ch <- fmt.Sprintf("while reading %s %v", url, err)
 		return
 	}
 	secs := time.Since(start).Seconds()
-	//ch <- fmt.Sprintf("%.2fs   %7d   %s", secs, nbytes, url)
-
-	ch <- fmt.Sprintf("%.2fs     %s", secs, url)
+	ch <- fmt.Sprintf("%.2fs   %7d   %s", secs, nbytes, url)
 }
